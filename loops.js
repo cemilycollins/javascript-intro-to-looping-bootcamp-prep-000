80341477020b72cd43a1a71f9beb5034fc58f1de
Emily Collins <c.emily.collins@gmail.com>
2018-05-10 01:44:42 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
-  for (var i = 1; i < 26; i++) {
-    if(i === 1) {
+  for (var i = 0; i < 25; i++) {
+    if(i === 0) {
        console.log("I am ${i) strange loop.")
     }
     else {
